67b1aab396e4d96128767b610d1b787d8cf9b912
Javigl <jgl83@alu.ua.es>
2022-09-10 18:57:24 +0200

    eureka

--- user-service/src/main/java/com/tutorial/userservice/feignClients/CarFeignClient.java
+++ user-service/src/main/java/com/tutorial/userservice/feignClients/CarFeignClient.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
-@FeignClient(name = "car-service", url = "http://localhost:8002/car")
+@FeignClient(name = "car-service")
 public interface CarFeignClient {
     @GetMapping("/byuser/{userId}")
     List<Car> getCars(@PathVariable("userId") int userId);
